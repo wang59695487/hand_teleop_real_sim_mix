626accb39d4b1046927af69f7f1dec9f2ad28494
wang59695487 <596954871@qq.com>
2023-08-23 23:05:10 -0700

    fix some bugs

--- main/train.py
+++ main/train.py
@@ -309,10 +309,6 @@ def eval_in_env(args, agent, log_dir, epoch, x_steps, y_steps):
 
 
 def main(args):
-    if not args["wandb_off"]:
-        wandb_cfg = OmegaConf.load("wandb_cfg.yaml")
-        wandb.login(key=wandb_cfg.key)
-
     # read and prepare data 
     Prepared_Data = prepare_real_sim_data(args['dataset_folder'],args["backbone_type"],args['batch_size'],args['batch_size'],
                                  args['val_ratio'], seed = 20230806)
@@ -364,12 +360,13 @@ def main(args):
     if not args["eval_only"]:
         cur_time = datetime.now().strftime("%Y%m%d-%H%M%S")
         log_dir = os.path.join("logs", f"{args['task']}_{args['backbone_type']}_{cur_time}")
-        if not args["wandb_off"]:
-            wandb.init(
-                project="hand-teleop",
-                name=os.path.basename(log_dir),
-                config=args
-            )
+        #wandb_cfg = OmegaConf.load("wandb_cfg.yaml")
+        #wandb.login(key=wandb_cfg.key)
+        wandb.init(
+            project="hand-teleop",
+            name=os.path.basename(log_dir),
+            config=args
+        )
         os.makedirs(log_dir, exist_ok=True)
         best_success = 0
         for epoch in range(args['num_epochs']):
@@ -425,45 +422,41 @@ def main(args):
                         train_policy=args['train_policy'], 
                         train_inv=args['train_inv'])
 
-            if not args["wandb_off"]:
-                wandb.log(metrics)
+            wandb.log(metrics)
 
         agent.load(os.path.join(log_dir, "epoch_best.pt"))
         agent.train(train_visual_encoder=False, train_state_encoder=False, train_policy=False, train_inv=False)
         final_success = eval_in_env(args, agent, log_dir, "best", 10, 10)
+
+        wandb.log({"final_success": final_success})
         print(f"Final success rate: {final_success:.4f}")
 
-        if not args["wandb_off"]:
-            wandb.log({"final_success": final_success})
-            wandb.finish()
+        wandb.finish()
     else:
         log_dir = os.path.dirname(args["ckpt"])
-        if not args["wandb_off"]:
-            wandb.init(
-                project="hand-teleop",
-                name=log_dir+args["ckpt"],
-                config=args
-            )
+        wandb.init(
+            project="hand-teleop",
+            name=log_dir+args["ckpt"],
+            config=args
+        )
         os.makedirs(log_dir, exist_ok=True)
         agent.load(args["ckpt"])
         agent.train(train_visual_encoder=False, train_state_encoder=False, train_policy=False, train_inv=False)
         final_success = eval_in_env(args, agent, log_dir, "best", 10, 10)
-        if not args["wandb_off"]:
-            wandb.log({"final_success": final_success})
-            wandb.finish()
-        print(f"Final success rate: {final_success:.4f}")        
+        wandb.log({"final_success": final_success})
+        print(f"Final success rate: {final_success:.4f}")
+
+        wandb.finish()
 
 
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument("--demo-folder", required=True)
-    parser.add_argument("--backbone-type", required=True)
+    parser.add_argument("--backbone-type", default="resnet50")
     parser.add_argument("--eval-freq", default=200, type=int)
     parser.add_argument("--eval-start-epoch", default=400, type=int)
     parser.add_argument("--eval-only", action="store_true")
     parser.add_argument("--ckpt", default=None, type=str)
-    parser.add_argument("--wandb-off", action="store_true")
-    parser.add_argument("--batch-size", default=65536, type=int)
     args = parser.parse_args()
 
     return args
@@ -474,7 +467,7 @@ if __name__ == '__main__':
 
     args = {
         'dataset_folder': args.demo_folder,
-        'batch_size': args.batch_size,
+        'batch_size': 65536,
         'val_ratio': 0.1,
         'bc_lr': 2e-5,
         'num_epochs': 1600,
@@ -515,8 +508,7 @@ if __name__ == '__main__':
         "eval_freq": args.eval_freq,
         "eval_start_epoch": args.eval_start_epoch,
         "eval_only": args.eval_only,
-        "ckpt": args.ckpt,
-        "wandb_off": args.wandb_off
+        "ckpt": args.ckpt
     }
     args = argument_dependecy_checker(args)
 
