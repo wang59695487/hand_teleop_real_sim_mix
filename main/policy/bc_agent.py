8ee5408428b0e609d92073dd51bd10ab3aae2094
wang59695487 <596954871@qq.com>
2023-09-06 03:10:04 -0700

    Set mse loss to sum instead of mean

--- main/policy/bc_agent.py
+++ main/policy/bc_agent.py
@@ -233,7 +233,7 @@ class BCSSAgent(object):
         # bc_loss.backward()
         # self.bc_module_optimizer.step()
 
-        return F.mse_loss(pred_action, action)
+        return F.mse_loss(pred_action, action, reduction='sum')
 
     
     def update(self, bc_loss, L=None, step=None, alter_lr=None):
